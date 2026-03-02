1c0282802b7cf91dddbfbd775a2f3ac3569bfa99
Muzammil Islam <muzammilislam31@gmail.com>
2026-03-02 14:47:22 +0500

    url update

--- src/components/HomeProducts.jsx
+++ src/components/HomeProducts.jsx
@@ -7,7 +7,7 @@ function Products() {
     const [showAll, setShowAll] = useState(false);
 
     useEffect(() => {
-        fetch(`https://shoesstore.infinityfreeapp.com/shoestore-backend/api/home-products.php`)
+        fetch(`shoestore-mi.infinityfreeapp.com/shoestore-backend/api/home-products.php`)
         .then(res => res.json())
         .then(data => setProduct(data))
         .catch(err => console.error(err));
